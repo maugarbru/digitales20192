baf5fa488a6c196eb9b2c6598917f12ca72cfe94
carlos <carlos@creativosdigitales.co>
2019-10-06 11:48:27 -0500

    probando consulta de postgres

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-const cors =  require("cors")
+const cors = require("cors")
 var express = require("express")
 var path = require("path")
 var { Client } = require('pg')
@@ -8,21 +8,26 @@ var client = new Client({
     connectionString: connectionString
 });
 // client.connect();
-var app =  express();
+var app = express();
 app.use(cors())
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + "/front/index.html"))
 })
-app.get('/about/',(req,res)=>{
+app.get('/about/', (req, res) => {
     res.sendFile(path.join(__dirname + "/front/about.html"))
 })
 app.get("/datos/", async function (req, res) {
-    res.status(200).send({ success: true, message: "Se ha realizado la peticion con exito" })
+    client.query('SELECT * FROM pruebas', [1], function (err, result) {
+        if (err) {
+            console.log(err);
+            res.status(400).send(err);
+        }
+        res.status(200).send(result.rows);
+    });
+});
+port = 8080
+app.listen(port, () => {
+    console.log(`Node is running in: http://localhost:${port}`);
 
 })
-port =  8080
-app.listen(port,()=>{
-    console.log(`Node is running in: http://localhost:${port}`);
-    
-} )
