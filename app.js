77398059210a19412f4603a9b3405cd97ea9e745
carlos <carlos@creativosdigitales.co>
2019-10-14 09:24:46 -0500

    cambiso en servidor

--- app.js
+++ app.js
@@ -8,18 +8,18 @@ var connectionString = 'postgres://equipo1:digitales123@localhost:5432/equipo1'
 var client = new Client({
     connectionString: connectionString
 });
-client.connect();
+// client.connect();
 var app = express();
 app.use(cors())
 app.use(bodyParser.json({ limit: "50mb" }));
 //support parsing of application/x-www-form-urlencoded post data
 app.use(
     bodyParser.urlencoded({
-      extended: true,
-      limit: "50mb",
-      parameterLimit: 50000
+        extended: true,
+        limit: "50mb",
+        parameterLimit: 50000
     })
-  );
+);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + "/front/index.html"))
@@ -43,8 +43,8 @@ app.get("/datos/", async function (req, res) {
  * uri que tiene la posibilidad de enviar los datos
  */
 app.post("/insertRegistro", function (req, res) {
-    let body =  req.body
-    let query =`insert  into  registros (uid_maquina,consumo,hora_inicio,hora_fin)  
+    let body = req.body
+    let query = `insert  into  registros (uid_maquina,consumo,hora_inicio,hora_fin)  
     values (${body.uid_maquina},${body.consumo},'${body.hora_inicio}', '${body.hora_fin}')`
     client.query(query, function (err, result) {
         if (err) {
@@ -54,6 +54,46 @@ app.post("/insertRegistro", function (req, res) {
         res.status(200).send("Se han agregado los datos satisfactoriamente");
     });
 });
+/**
+ * query para filtro por fechas de los reportes
+ *  params:
+ * - recibe como parametro dos fechas especificas que son fecha_inicio  y fecha_fin
+ */
+app.get("/filtroFecha?", function (req, res) {
+    let params = req.query
+    let query = `select * from  registros 
+    where to_timestamp(hora_inicio,'YYYY-MM-DD') between '${params.fecha_inicio}'  and  '${params.fecha_fin}'`
+
+    res.status(200).send(query)
+    client.query(query, function (err, result) {
+        if (err) {
+            console.log(err);
+            res.status(400).send(err);
+        }
+        res.status(200).send("Se han agregado los datos satisfactoriamente");
+    });
+});
+/**
+ * query para filtro por fechas de los reportes
+ *  params:
+ * - recibe como parametro una fechas especificas que es fecha 
+ */
+app.get("/filtroDia?", function (req, res) {
+    let params = req.query
+    let query = `select  * from registros 
+	where  hora_inicio  like '${params.fecha}%'`
+    console.log();
+    res.status(200).send(query)
+    client.query(query, function (err, result) {
+        if (err) {
+            console.log(err);
+            res.status(400).send(err);
+        }
+        res.status(200).send("Se han agregado los datos satisfactoriamente");
+    });
+});
+
+
 port = 8080
 app.listen(port, () => {
     console.log(`Node is running in: http://localhost:${port}`);
