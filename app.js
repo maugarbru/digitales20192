254b5e4aa0d704deb523c707b71524a52238d31b
carlos <carlos@creativosdigitales.co>
2019-11-06 17:22:40 -0500

    cambios en autenticacion

--- app.js
+++ app.js
@@ -3,177 +3,241 @@ const cors = require("cors")
 var express = require("express")
 var path = require("path")
 var bodyParser = require("body-parser");
+var jwt = require('jsonwebtoken')
 var { Client } = require('pg')
 var connectionString = 'postgres://equipo1:digitales123@localhost:5432/equipo1'
+var exceptionToken = ["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaGVjayI6dHJ1ZSwiaWF0IjoxNTczMDU5NTMxLCJleHAiOjE1NzMwNjA5NzF9.lluicOnBST8G2CA428CZdvlQeYFPMHX4zXhOzHbPU8E"]
 var client = new Client({
-    connectionString: connectionString
+	connectionString: connectionString
 });
+const rutasProtegidas = express.Router();
 client.connect();
 var app = express();
+app.set("llave", "digitales123")
 app.use(cors())
 app.use(bodyParser.json({ limit: "50mb" }));
 //support parsing of application/x-www-form-urlencoded post data
 app.use(
-    bodyParser.urlencoded({
-        extended: true,
-        limit: "50mb",
-        parameterLimit: 50000
-    })
+	bodyParser.urlencoded({
+		extended: true,
+		limit: "50mb",
+		parameterLimit: 50000
+	})
 );
 
+rutasProtegidas.use((req, res, next) => {
+	const token = req.headers['access-token'];
+	if (exceptionToken.filter(item => item == token).length > 0) {
+		// res.send({
+
+		next();
+		// });
+	}
+	if (token) {
+		jwt.verify(token, app.get('llave'), (err, decoded) => {
+			if (err) {
+				return res.json({ mensaje: 'Token inválida' });
+			} else {
+				req.decoded = decoded;
+				next();
+			}
+		});
+	} else {
+		res.send({
+			mensaje: 'Token no proveída.'
+		});
+	}
+});
+
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + "/front/index.html"))
+	res.sendFile(path.join(__dirname + "/front/index.html"))
 })
 app.get('/about/', (req, res) => {
-    res.sendFile(path.join(__dirname + "/front/about.html"))
+	res.sendFile(path.join(__dirname + "/front/about.html"))
 })
 app.get('/login/', (req, res) => {
-    res.sendFile(path.join(__dirname + "/front/login.html"))
+	res.sendFile(path.join(__dirname + "/front/login.html"))
+})
+app.get('/GestorMaquinas/', (req, res) => {
+	res.sendFile(path.join(__dirname + "/front/GestorMaquinas.html"))
+})
+/**
+ *
+ *  query para obtener las maquinas
+ */
+// app.get("/loginUser", function (req, res) {
+//     let params = req.query
+//     let query = `select nombre from usuarios where id = '${params.id}' and password = '${params.password}'`
+//     client.query(query, function (err, result) {
+//         if (err) {
+//             console.log(err);
+//             res.status(400).send(err);
+//         }
+//         res.status(200).send(result.rows);
+//     });
+// });
+
+
+
+/**
+ * autenticación de usuarios
+ */
+app.post('/autenticar?', (req, res) => {
+	var params = req.query
+	console.log(params.contraseña);
+
+	let query = `select id,password from usuarios where id = '${params.id}' and password = '${params.password}'`
+	client.query(query, function (err, result) {
+		if (err) {
+			console.log(err);
+			res.status(400).send(err);
+		}
+		if (params.usuario === result.id && params.contraseña === result.password) {
+			const payload = {
+				check: true
+			};
+			const token = jwt.sign(payload, app.get('llave'), {
+				expiresIn: 1440
+			});
+			res.json({
+				mensaje: 'Autenticación correcta',
+				token: token
+			});
+		} else {
+			res.json({ mensaje: "Usuario o contraseña incorrectos" })
+		}
+	});
+
 })
 /**
  * uri con las opciones de traer todos los registros
  */
 app.get("/datos/", async function (req, res) {
-    client.query('SELECT * FROM registros', function (err, result) {
-        if (err) {
-            console.log(err);
-            res.status(400).send(err);
-        }
-        res.status(200).send(result.rows);
-    });
+	client.query('SELECT * FROM registros', function (err, result) {
+		if (err) {
+			console.log(err);
+			res.status(400).send(err);
+		}
+		res.status(200).send(result.rows);
+	});
 });
 /**
  * uri que tiene la posibilidad de enviar los datos
  */
 app.post("/insertRegistro", function (req, res) {
-    let body = req.body
-    let query = `insert  into  registros (uid_maquina,consumo,hora_inicio,hora_fin)  
+	let body = req.body
+	let query = `insert  into  registros (uid_maquina,consumo,hora_inicio,hora_fin)  
     values (${body.uid_maquina},${body.consumo},'${body.hora_inicio}', '${body.hora_fin}')`
-    client.query(query, function (err, result) {
-        if (err) {
-            console.log(err);
-            res.status(400).send(err);
-        }
-        res.status(200).send("Se han agregado los datos satisfactoriamente");
-    });
+	client.query(query, function (err, result) {
+		if (err) {
+			console.log(err);
+			res.status(400).send(err);
+		}
+		res.status(200).send("Se han agregado los datos satisfactoriamente");
+	});
 });
 /**
  * query para filtro por fechas de los reportes
  *  params:
  * - recibe como parametro dos fechas especificas que son fecha_inicio  y fecha_fin
  */
-app.get("/filtroFecha?", function (req, res) {
-    let params = req.query
-    // let query = `select * from  registros 
-    // where to_timestamp(hora_inicio,'YYYY-MM-DD') between '${params.fecha_inicio}'  and  '${params.fecha_fin}'` 
-    let query = `select substring(hora_inicio from 0 for 11) as fecha, round(sum(consumo),2) as consumo  from registros
+app.get("/filtroFecha?", rutasProtegidas, function (req, res) {
+	let params = req.query
+	let query = `select substring(hora_inicio from 0 for 11) as fecha, round(sum(consumo),2) as consumo  from registros
     where uid_maquina = ${params.uid} and to_timestamp(hora_inicio,'YYYY-MM-DD') between '${params.fecha_inicio}'  and  '${params.fecha_fin}'
     group by  substring(hora_inicio from 0 for 11) order by substring(hora_inicio from 0 for 11)`
-    client.query(query, function (err, result) {
-        if (err) {
-            console.log(err);
-            res.status(400).send(err);
-        }
-        res.status(200).send(result.rows);
-    });
+	client.query(query, function (err, result) {
+		if (err) {
+			console.log(err);
+			res.status(400).send(err);
+		}
+		res.status(200).send(result.rows);
+	});
 });
 /**
  * query para filtro por fechas de los reportes
  *  params:
  * - recibe como parametro una fechas especificas que es fecha 
  */
-app.get("/filtroDia?", function (req, res) {
-    let params = req.query
-    let query = `select  * from registros 
+app.get("/filtroDia?", rutasProtegidas, function (req, res) {
+	let params = req.query
+	let query = `select  * from registros 
     where uid_maquina = ${params.uid} and hora_inicio like '${params.fecha}%' order by hora_fin`
-    client.query(query, function (err, result) {
-        if (err) {
-            console.log(err);
-            res.status(400).send(err);
-        }
-        res.status(200).send(result.rows);
-    });
+	client.query(query, function (err, result) {
+		if (err) {
+			console.log(err);
+			res.status(400).send(err);
+		}
+		res.status(200).send(result.rows);
+	});
 });
 
 /**
  * query para obtener las maquinas
  */
-app.get("/maquinas", function (req, res) {
-    let query = `select  * from maquinas`
-    client.query(query, function (err, result) {
-        if (err) {
-            console.log(err);
-            res.status(400).send(err);
-        }
-        res.status(200).send(result.rows);
-    });
+app.get("/maquinas", rutasProtegidas, function (req, res) {
+	let query = `select  * from maquinas`
+	client.query(query, function (err, result) {
+		if (err) {
+			console.log(err);
+			res.status(400).send(err);
+		}
+		res.status(200).send(result.rows);
+	});
 });
 
 /**
  * query para insert las maquinas
  */
 app.post("/maquinas", function (req, res) {
-    let body = req.body
-    let query = `insert into maquinas (codigo, nombre, ubicacion, descripcion) values ('${body.codigo}', '${body.nombre}', '${body.ubicacion}', '${body.descripcion}')`
-    console.log(query);
-    client.query(query, function (err, result) {
-        if (err) {
-            console.log(err);
-            res.status(400).send(err);
-        }
-        res.status(200).send("Se ha agregado la maquina correctamente");
-    });
+	let body = req.body
+	let query = `insert into maquinas (codigo, nombre, ubicacion, descripcion) values ('${body.codigo}', '${body.nombre}', '${body.ubicacion}', '${body.descripcion}')`
+	console.log(query);
+	client.query(query, function (err, result) {
+		if (err) {
+			console.log(err);
+			res.status(400).send(err);
+		}
+		res.status(200).send("Se ha agregado la maquina correctamente");
+	});
 });
 
 /**
  * query para update las maquinas
  */
 app.put("/maquinas", function (req, res) {
-    let body = req.body
-    let query = `update maquinas set codigo = '${body.codigo}', nombre = '${body.nombre}', ubicacion = '${body.ubicacion}', descripcion = '${body.descripcion}' where uid = ${body.uid}`
-    console.log(query);
-    client.query(query, function (err, result) {
-        if (err) {
-            console.log(err);
-            res.status(400).send(err);
-        }
-        res.status(200).send("Se ha actualizado la maquina correctamente");
-    });
+	let body = req.body
+	let query = `update maquinas set codigo = '${body.codigo}', nombre = '${body.nombre}', ubicacion = '${body.ubicacion}', descripcion = '${body.descripcion}' where uid = ${body.uid}`
+	console.log(query);
+	client.query(query, function (err, result) {
+		if (err) {
+			console.log(err);
+			res.status(400).send(err);
+		}
+		res.status(200).send("Se ha actualizado la maquina correctamente");
+	});
 });
 
 /**
  * query para delete las maquinas
  */
 app.delete("/maquinas", function (req, res) {
-    let params = req.query
-    let query = `delete from maquinas where uid = ${params.uid} `
-    client.query(query, function (err, result) {
-        if (err) {
-            console.log(err);
-            res.status(400).send(err);
-        }
-        res.status(200).send("Se ha eliminado la maquina correctamente");
-    });
+	let params = req.query
+	let query = `delete from maquinas where uid = ${params.uid} `
+	client.query(query, function (err, result) {
+		if (err) {
+			console.log(err);
+			res.status(400).send(err);
+		}
+		res.status(200).send("Se ha eliminado la maquina correctamente");
+	});
 });
 
-/**
- * query para obtener las maquinas
- */
-app.get("/loginUser", function (req, res) {
-    let params = req.query
-    let query = `select nombre from usuarios where id = '${params.id}' and password = '${params.password}'`
-    client.query(query, function (err, result) {
-        if (err) {
-            console.log(err);
-            res.status(400).send(err);
-        }
-        res.status(200).send(result.rows);
-    });
-});
 
 
 port = 8080
 app.listen(port, () => {
-    console.log(`Node is running in: http://localhost:${port}`);
+	console.log(`Node is running in: http://localhost:${port}`);
 
 })
