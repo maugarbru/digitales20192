3caf4ec928d46ad1750cf5c07ac5faa2cd0959b3
carlos <carlos@creativosdigitales.co>
2019-11-06 18:10:05 -0500

    cambios en autenticacion

--- app.js
+++ app.js
@@ -93,12 +93,12 @@ app.post('/autenticar?', (req, res) => {
 	client.query(query, function (err, result) {
 		console.log(result)
 		console.log(query)
-		var user = result.rows[0]
 		if (err) {
 			console.log(err);
 			res.status(400).send(err);
 		}
-		if (params.usuario === user.id && params.contraseña === user.password) {
+
+		if (result.rows.length>0) {
 			const payload = {
 				check: true
 			};
